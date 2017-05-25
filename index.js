0389d62809c1c9093fc44abe844f664d8f50a53c
whatwewant <tobewhatwewant@gmail.com>
2017-05-25 21:46:54 +0800

    ???

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 * @Date:   2017-04-12T07:45:29+08:00
 * @Email:  uniquecolesmith@gmail.com
 * @Last modified by:   eason
-* @Last modified time: 2017-05-25T20:35:19+08:00
+* @Last modified time: 2017-05-25T21:46:41+08:00
 * @License: MIT
 * @Copyright: Eason(uniquecolesmith@gmail.com)
 */
@@ -15,6 +15,7 @@ const routes = require('./routes');
 
 const app = Eva({
   server: {
+    HOST: 'localhost',
     PORT: process.env.PORT || 5000
   },
   db: null,
@@ -28,8 +29,8 @@ app.register({
 
 app.start();
 
-// app.set('port', process.env.PORT || 5000);
+// app.instance.set('port', process.env.PORT || 5000);
 //
-// app.listen(app.get('port'), () => {
-//   console.log('Node app is running on port', app.get('port'));
+// app.instance.listen(app.instance.get('port'), () => {
+//   console.log('Node app is running on port', app.instance.get('port'));
 // });
