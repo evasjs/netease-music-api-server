03521a971e56e2f0bd5c2d077c586e1acc6dc1b7
tobewhatwewant <tobewhatwewant@gmail.com>
2017-07-06 07:23:07 +0800

    :sparkles:(Graphql:Query:music) add music port

--- graphql.js
+++ graphql.js
@@ -3,26 +3,40 @@ const { GraphQLScalarType } = require('graphql');
 const fetch = require('node-fetch');
 const api = require('./lib')();
 
+const prefix = `http://127.0.0.1:${process.env.PORT || 5000}/v1`;
+
 module.exports = {
   schemas: `
     scalar JSON
 
     type Query {
       common(path: String!): JSON
+    
+      music(id: Int!, type: String = "url", br: Int = 128000): JSON
     }
   `,
   resolvers: {
     async common({ path }) {
       if (!(path.split('?')[0] in api)) {
         return {
-          code: -1,
+          code: 0X0001,
           msg: 'invalid services',
         };
       }
 
-      const res = await fetch(`http://127.0.0.1:${process.env.PORT || 5000}/v1${path}`);
+      const res = await fetch(`${prefix}${path}`);
       const json = await res.json();
       return json;
+    },
+    async music({ id, type, br }) {
+      if (!['url', 'detail'].includes(type)) {
+        return {
+          code: 0X0002,
+          msg: 'invalid type',
+        };
+      }
+      const res = await fetch(`${prefix}/music/${type}?id=${id}&br=${br}`)
+      return res.json();
     },
   },
   types: {
